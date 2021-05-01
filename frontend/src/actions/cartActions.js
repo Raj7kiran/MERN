cfa0dcb346aa726ee93ea341c7d8a5a789ef3f2d
Rajkiran <raj7kiran@gmail.com>
2021-05-01 20:06:57 +0530

    Add Cart - Actions created

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { CART_ADD_IETM } from '../constants/cartConstants'
+
+
+//id and qty comesfrom the URL
+//asyncfunction in all actions is from thonk
+//getState gets the details from reducers in store.js
+export const addToCart = (id,qty) => async (dispatch, getState) => {
+	const { data } = await axios.get(`/api/products/${id}`)
+
+	dispatch({
+		type: CART_ADD_IETM,
+		payload: {
+			product: data_.id,
+			name: data.name,
+			image: data.image,
+			price: data.price,
+			countInStock: data.countInStock,
+			qty,
+		}
+	})
+	//is used to store datain local storage and it takes data in string not JSON
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
