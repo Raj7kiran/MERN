c6b6810335efc203f41fcb4c03c005a8f40970c6
Rajkiran <raj7kiran@gmail.com>
2021-05-06 21:21:18 +0530

    User registertration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,42 @@ const authUser = asyncHandler(async(req, res) => {
 })
 
 
+//.@desc create a user
+//.@route POST /api/users
+//.@access Public
+const registerUser = asyncHandler(async(req, res) => {
+	const {name, email, password} = req.body
+
+	const userExists = await User.findOne({ email })
+
+	if(userExists){
+		res.status(400)
+		throw new Error('User email already exists');
+	}
+
+	//here i guess he is using ES6 syntax
+	const user = await User.create({
+		name,
+		email,
+		password
+	})
+
+	if(user){
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id)
+		})
+	} else{
+		res.status(400)
+		throw new Error('Invalid User Data')
+	}
+
+})
+
+
 //.@desc getuser profile
 //.@route GET /api/users/profile
 //.@access Private
@@ -46,4 +82,4 @@ const getUserProfile = asyncHandler(async (req,res) => {
 
 
 
-export {authUser, getUserProfile}
+export {authUser, registerUser, getUserProfile}
